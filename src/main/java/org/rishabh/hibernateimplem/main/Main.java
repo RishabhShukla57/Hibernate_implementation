823c326887411bd1d5b66558ffce2046c969d0f0
Rishabhjaypee <rishabhjiit62@gmail.com>
2016-10-24 23:47:19 +0530

    First Commit

---
+++ src/main/java/org/rishabh/hibernateimplem/main/Main.java
@@ -0,0 +1,24 @@
+package org.rishabh.hibernateimplem.main;
+
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.rishabh.hibernateimplem.model.Stock;
+import org.rishabh.hibernateimplem.persistence.HibernateUtil;
+
+public class Main {
+	public static void main(String [] args){
+		Stock stock = new Stock();
+		stock.setStockCode("AMB");
+		stock.setStockName("AMBUJA CEMENT");
+		stock.setStockPrice(923);
+		SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
+		Session session  =  sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(stock);
+		session.getTransaction().commit();
+		sessionFactory.close();
+		
+	}
+
+}
---
+++ src/main/java/org/rishabh/hibernateimplem/model/Stock.java
@@ -0,0 +1,45 @@
+package org.rishabh.hibernateimplem.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Stock implements java.io.Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	int stockId;
+	String stockCode;
+	String stockName;
+	int stockPrice;
+	public int getStockId() {
+		return stockId;
+	}
+	public void setStockId(int stockId) {
+		this.stockId = stockId;
+	}
+	public String getStockCode() {
+		return stockCode;
+	}
+	public void setStockCode(String stockCode) {
+		this.stockCode = stockCode;
+	}
+	public String getStockName() {
+		return stockName;
+	}
+	public void setStockName(String stockName) {
+		this.stockName = stockName;
+	}
+	public int getStockPrice() {
+		return stockPrice;
+	}
+	public void setStockPrice(int stockPrice) {
+		this.stockPrice = stockPrice;
+	}
+}
